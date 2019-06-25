bdb08d5fccdc26469b7a037ac5b5e36518733cb1
ElenaKikahina <amikla-@mail.ru>
2019-06-25 15:37:24 +0600

    fixed tests

--- PycharmProjects/behave/features/environment.py
+++ PycharmProjects/behave/features/environment.py
@@ -23,11 +23,11 @@ def before_scenario(context, scenario):
         BROWSER = 'chrome'
     if BROWSER == 'chrome':
         chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument('--headless')
-        # chrome_options.add_argument('--no-sandbox')
-        # chrome_options.add_argument('--start-maximized')
-        # chrome_options.add_argument('--disable-extensions')
-        # chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--start-maximized')
+        chrome_options.add_argument('--disable-extensions')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         context.browser = webdriver.Chrome(chrome_options=chrome_options)
     # context.browser = webdriver.Chrome()
     elif BROWSER == 'firefox':
